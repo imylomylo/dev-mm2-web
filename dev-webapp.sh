b4bc5b8d828df3a1cc6a86ce1d45e101c6444622
MyloHDEX <mylo@hybriddex.com>
2020-05-03 12:59:18 +0000

    title

--- dev-webapp.sh
+++ dev-webapp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #VUE_APP_WEBHOST=demo.marketmaker.club
 #VUE_APP_WEBPORT=7779
-echo "VUE_APP_MMBOTHOST=127.0.0.1">.env.local
-echo "VUE_APP_MMBOTPORT=7780">>.env.local
+#echo "VUE_APP_MMBOTHOST=127.0.0.1">.env.local
+#echo "VUE_APP_MMBOTPORT=7780">>.env.local
 npm run serve
